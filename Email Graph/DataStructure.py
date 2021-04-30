84f969edcb628c209b7d07b2757f63a27109e5c6
Victor Asanache <victor.asanache@gmail.com>
2021-04-30 10:53:06 +0300

    Graph Data Structure

---
+++ Email Graph/DataStructure.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import networkx as nx 
+
+mailSet = pd.read_csv("enron-v1.csv")
+
+mailGraph = nx.from_pandas_edgelist(mailSet, 'fromId', 'toId', ['fromEmail', 'fromJobtitle', 'toEmail', 'toJobtitle', 'messageType', 'sentiment', 'date'], create_using = nx.MultiDiGraph())
+
+for edge in mailGraph.edges:
+    #print(edge)
+    edgeAttribute = mailGraph.get_edge_data(*edge)
+    print(edgeAttribute)
+    mailGraph.nodes[edge[0]]['Email'] = edgeAttribute['fromEmail']
+    mailGraph.nodes[edge[0]]['Job'] = edgeAttribute['fromJobtitle']
+    mailGraph.nodes[edge[1]]['Email'] = edgeAttribute['toEmail']
+    mailGraph.nodes[edge[1]]['Job'] = edgeAttribute['toJobtitle']
+
+#print(mailGraph.nodes(data=True))
+
+#numberofNodes = mailGraph.number_of_nodes()
+for node in mailGraph.nodes:
+    print(node, mailGraph.nodes[node]["Email"], mailGraph.nodes[node]["Job"])
