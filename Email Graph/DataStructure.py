c2b448181f2f50566ba97b3a2e1c2c83d47c2ec9
Victor Asanache <victor.asanache@gmail.com>
2021-05-01 16:51:30 +0300

    Optimized writting node attributes

--- Email Graph/DataStructure.py
+++ Email Graph/DataStructure.py
@@ -8,14 +8,17 @@ mailGraph = nx.from_pandas_edgelist(mailSet, 'fromId', 'toId', ['fromEmail', 'fr
 for edge in mailGraph.edges:
     #print(edge)
     edgeAttribute = mailGraph.get_edge_data(*edge)
-    print(edgeAttribute)
-    mailGraph.nodes[edge[0]]['Email'] = edgeAttribute['fromEmail']
-    mailGraph.nodes[edge[0]]['Job'] = edgeAttribute['fromJobtitle']
-    mailGraph.nodes[edge[1]]['Email'] = edgeAttribute['toEmail']
-    mailGraph.nodes[edge[1]]['Job'] = edgeAttribute['toJobtitle']
+    #print(edgeAttribute)
+    if(edge[2] == 0):
+        if(mailGraph.nodes[edge[0]].get('Email') is None):
+            mailGraph.nodes[edge[0]]['Email'] = edgeAttribute['fromEmail']
+            mailGraph.nodes[edge[0]]['Job'] = edgeAttribute['fromJobtitle']
+        if(mailGraph.nodes[edge[1]].get('Email') is None):
+            mailGraph.nodes[edge[1]]['Email'] = edgeAttribute['toEmail']
+            mailGraph.nodes[edge[1]]['Job'] = edgeAttribute['toJobtitle']
 
 #print(mailGraph.nodes(data=True))
 
 #numberofNodes = mailGraph.number_of_nodes()
-for node in mailGraph.nodes:
-    print(node, mailGraph.nodes[node]["Email"], mailGraph.nodes[node]["Job"])
+#for node in mailGraph.nodes:
+    #print(node, mailGraph.nodes[node]["Email"], mailGraph.nodes[node]["Job"])
