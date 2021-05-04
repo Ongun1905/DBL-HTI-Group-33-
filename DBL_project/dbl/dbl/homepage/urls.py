41f4d511d8ac96c98635ef9938277192ee09ffda
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2021-05-04 13:18:33 +0200

    Best practices added

--- DBL_project/dbl/dbl/homepage/urls.py
+++ DBL_project/dbl/dbl/homepage/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about", views.about, name="about"),
-    path("index", views.index, name="index"), #You can also link with '..\' in about.html instead of 'index', but idk whats cleaner
 ]
