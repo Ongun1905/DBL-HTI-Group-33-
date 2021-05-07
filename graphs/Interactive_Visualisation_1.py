013de56e416c7aee73888474beb39452f6c6405d
Victor Asanache <victor.asanache@gmail.com>
2021-05-07 22:37:55 +0300

    Added al interactions, positioning still required for task completion

--- graphs/Interactive_Visualisation_1.py
+++ graphs/Interactive_Visualisation_1.py
@@ -10,7 +10,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 from colour import Color
-from datetime import datetime
+from datetime import date
 from textwrap import dedent as d
 import json
 import math
@@ -26,20 +26,18 @@ import math
 sentimentRange = [-1, 1]
 jobFromRange = []
 jobToRange = []
+mailFromRange = []
+mailToRange = []
+toccSelect = []
+showhideNodes = True
 
-# Read CSV and setup NX graph data structure
-mailSet = pd.read_csv("enron-v1.csv", engine='python')
-mailSet['date'] = pd.to_datetime(mailSet['date']) # Filter the date for Dash
-
-# Generate graph from CSV information
-mailGraph = nx.from_pandas_edgelist(mailSet, 'fromId', 'toId', ['fromEmail', 'fromJobtitle', 'toEmail', 'toJobtitle', 'messageType', 'sentiment', 'date'], create_using = nx.MultiDiGraph() )
+input_file = "enron-v1.csv"
 
 # Set up initial graph with positions and node attributes
-vis1Graph, jobFrom, jobTo = nlf.createGraph(mailGraph)
+vis1Graph, jobFrom, jobTo, mailFrom, mailTo, minDate, maxDate = nlf.createGraph(input_file)
 
-
-# Filter the graph
-#vis1GraphFilter = nlf.filterGraph(vis1Graph, sentimentRange)
+dateStart = minDate 
+dateEnd = maxDate
 
 # Get external styles for the Dash app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -48,55 +46,155 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Email Network"
 
+####
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
 # Generating Layout
 app.layout = html.Div([
-    dcc.RangeSlider(
-        id='my-range-slider',
-        min=-1,
-        max=1,
-        step=0.01,
-        marks={
-        -1: {'label': '-1: Very Negative Email', 'style': {'color': '#f50'}},
-        0: {'label': '0: Neutral Email'},
-        1: {'label': '1: Very Positive Email', 'style': {'color': '#77b0b1'}},
-        },
-        value=[-1, 1],
-        allowCross=False
-    ),
-    dcc.Dropdown(
-        id='jobFrom-dropdown',
-        options=[
-            {'label': j, 'value': j} for j in sorted(jobFrom)
-        ],
-        multi = True,
-        placeholder="Select from Job, Nothing = all"
-    ),
-    dcc.Dropdown(
-        id='jobTo-dropdown',
-        options=[
-            {'label': j, 'value': j} for j in sorted(jobTo)
-        ],
-        multi = True,
-        placeholder="Select to Job, Nothing = all"
-    ),
-    #html.Div(id='output-container-range-slider'),
     html.Div(
-        children=[dcc.Graph(id="mail-graph", figure=nlf.filterGraph(vis1Graph, sentimentRange, jobFromRange, jobToRange))]
+        children = [
+            html.Div(
+                children = [
+                    dcc.RangeSlider(
+                        id='my-range-slider',
+                        min=-1,
+                        max=1,
+                        step=0.01,
+                        marks={
+                        -1: {'label': ': Very Negative Email : - 1', 'style': {'color': '#f50'}},
+                        0: {'label': '0: Neutral Email'},
+                        1: {'label': '1: Very Positive Email', 'style': {'color': '#77b0b1'}},
+                        },
+                        value=[-1, 1],
+                        allowCross=False,   
+                    ),
+                    html.Br(),#breaks space between the interactive elements
+                    html.Br(),
+                ],
+                style = {'width': '400px'}
+            ),
+            
+            html.Div(
+                children = [
+                    dcc.Dropdown(
+                        id='jobFrom-dropdown',
+                        options=[
+                            {'label': j, 'value': j} for j in sorted(jobFrom)
+                        ],
+                        multi = True,
+                        placeholder="Select from Job, Nothing = all"
+                    ),
+
+                    dcc.Dropdown(
+                        id='jobTo-dropdown',
+                        options=[
+                            {'label': j, 'value': j} for j in sorted(jobTo)
+                        ],
+                        multi = True,
+                        placeholder="Select to Job, Nothing = all"
+                    ),
+                ],
+                style = {'width': '300px'}
+            ),
+
+            html.Div(
+                children = [
+                    dcc.Dropdown(
+                        id='mailFrom-dropdown',
+                        options=[
+                            {'label': j, 'value': j} for j in sorted(mailFrom)
+                        ],
+                        multi = True,
+                        placeholder="Select from Email, Nothing = all"
+                    ),
+
+                    dcc.Dropdown(
+                        id='mailTo-dropdown',
+                        options=[
+                            {'label': j, 'value': j} for j in sorted(mailTo)
+                        ],
+                        multi = True,
+                        placeholder="Select to Email, Nothing = all"
+                    ),
+                ],
+                style = {'width': '300px'}
+            ),
+
+            html.Div(
+                dcc.DatePickerRange(
+                    id='mail-date-range',
+                        min_date_allowed=minDate,
+                        max_date_allowed=maxDate,
+                        initial_visible_month=minDate,
+                        start_date = minDate,
+                        end_date = maxDate,
+                        start_date_placeholder_text="MMM Do, YYYY",
+                        end_date_placeholder_text="MMM Do, YYYY",
+                        first_day_of_week = 2,
+                        display_format='MMM Do, YYYY'
+                ) 
+            ),
+
+            html.Div(
+                dcc.Checklist(
+                    id = 'to-cc-checklist',
+                    options=[
+                        {'label': 'TO', 'value': 'TO'},
+                        {'label': 'CC', 'value': 'CC'}
+                    ],
+                    value=['TO', 'CC'],
+                    labelStyle={'display': 'inline-block'}
+                ) 
+            ),
+
+                html.Div(
+                    dcc.RadioItems(
+                    id = 'node-radio-items',
+                    options=[
+                        {'label': 'Show unlinked nodes', 'value': 'True'},
+                        {'label': 'Hyde unlinked nodes', 'value': 'False'}
+                    ],
+                    value='True',
+                    labelStyle={'display': 'inline-block'}
+                )
+            )
+        ]    
+    ),
+
+    html.Div(
+        children=[dcc.Graph(id="mail-graph", 
+        figure=nlf.filterGraph(vis1Graph, sentimentRange, jobFromRange, jobToRange, mailFromRange, mailToRange, dateStart, dateEnd, toccSelect, showhideNodes))]
     )
 ])
 
 @app.callback(
-    # Sentiment range slider input
-    #dash.dependencies.Output('output-container-range-slider', 'children'),
-    dash.dependencies.Output('mail-graph', 'figure'),
-    [dash.dependencies.Input('my-range-slider', 'value'), dash.dependencies.Input('jobFrom-dropdown', 'value'), dash.dependencies.Input('jobTo-dropdown', 'value')]
-)
-def update_output(value, jobFromInput, jobToInput):
-    #return 'You have selected the sentiment interval {}'.format(value)
+     dash.dependencies.Output('mail-graph', 'figure'),
+     [dash.dependencies.Input('my-range-slider', 'value'), 
+      dash.dependencies.Input('jobFrom-dropdown', 'value'), 
+      dash.dependencies.Input('jobTo-dropdown', 'value'), 
+      dash.dependencies.Input('mailFrom-dropdown', 'value'), 
+      dash.dependencies.Input('mailTo-dropdown', 'value'), 
+      dash.dependencies.Input('mail-date-range', 'start_date'),
+      dash.dependencies.Input('mail-date-range', 'end_date'),
+      dash.dependencies.Input('to-cc-checklist', 'value'),
+      dash.dependencies.Input('node-radio-items', 'value')])
+
+def update_output(value, jobFromInput, jobToInput, mailFromInput, mailToInput, mailStartDate, mailEndDate, tocc, showhide):
     sentimentRange = value
     jobFromRange = jobFromInput
     jobToRange = jobToInput
-    return nlf.filterGraph(vis1Graph, sentimentRange, jobFromRange, jobToRange)
+    mailFromRange = mailFromInput
+    mailToRange = mailToInput
+    dateStart = pd.to_datetime(mailStartDate)
+    dateEnd = pd.to_datetime(mailEndDate)
+    toccSelect = tocc
+    showhideNodes = showhide
+    return nlf.filterGraph(vis1Graph, sentimentRange, jobFromRange, jobToRange, mailFromRange, mailToRange, dateStart, dateEnd, toccSelect, showhideNodes)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
