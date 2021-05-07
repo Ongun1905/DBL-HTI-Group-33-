a7211773e39b030134993045a8cfc999c0d3f9fc
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2021-05-07 14:57:31 +0200

    vis1 site
    I added vis on site

---
+++ DBL_project/dbl/homepage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from homepage.dash_apps.finished_apps import simple_example
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("vis1", views.vis1, name = "vis1"),
+]
