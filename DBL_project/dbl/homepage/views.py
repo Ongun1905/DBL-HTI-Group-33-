655d69b8b6a0250a95bead5549ab4393f0f0e0c6
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2021-05-27 18:18:34 +0200

    uploaded button fixed

--- DBL_project/dbl/homepage/views.py
+++ DBL_project/dbl/homepage/views.py
@@ -6,20 +6,31 @@ from django.http import HttpResponse
 from .visualizations import adjacency_matrix
 from django.core.files.storage import FileSystemStorage
 import os
+from django import forms
 
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
 # Create your views here.
 def index(request): 
     if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        if uploaded_file.name in os.listdir(settings.BASE_DIR / 'media'):
-            name = os.listdir(settings.BASE_DIR / 'media')[-1][:-4] + '1.csv'
-        else:
-            name = uploaded_file.name
-        filename = fs.save(name, uploaded_file)
-        uploaded_file_url = fs.url(filename)
-        return render(request, "homepage/index.html", {"uploaded_file_url":"uploaded file: " + filename} )
-    return render(request, "homepage/index.html")
+        # Take it data the user sbmitted and save it as a form 
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = request.FILES['file']
+            fs = FileSystemStorage()
+            if uploaded_file.name in os.listdir(settings.BASE_DIR / 'media'):
+                name = os.listdir(settings.BASE_DIR / 'media')[-1][:-4] + '1.csv'
+            else:
+                name = uploaded_file.name
+            filename = fs.save(name, uploaded_file)
+            uploaded_file_url = fs.url(filename)
+            return render(request, "homepage/index.html", {
+                "uploaded_file_url":"uploaded file: " + filename,
+                "form":form})
+    
+    return render(request, "homepage/index.html", {
+        "uploaded_file_url":"You have not uploaded any file", 
+        "form":UploadFileForm()})
 
 def about(request):
     return render(request, "homepage/about.html")
