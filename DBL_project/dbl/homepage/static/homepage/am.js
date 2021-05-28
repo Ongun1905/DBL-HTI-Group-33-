7b0c313976df116db4f8df09acba0ab538649642
PXJesse <44534584+PXJesse@users.noreply.github.com>
2021-05-28 14:03:29 +0200

    💡 Added modal concept
    Added a click interaction to the adjacency matrix that opens a modal. The idea is that this modal will contain a list of all emails with their sentiment values, which is very useful in the case that there are multiple emails between two employees.

--- DBL_project/dbl/homepage/static/homepage/am.js
+++ DBL_project/dbl/homepage/static/homepage/am.js
@@ -1,12 +1,12 @@
 /**
  * Opens the popup that displays when hovering a cell in the adjacency matrix
- * @param {Event} e The hover event
+ * @param {HTMLElement} matrixCell The cell that was hovered
  * @param {Array.<Object>} nodeInfo An array of node objects
  */
-function enterCell(e, matrixCell, nodeInfo) {
+function enterCell(matrixCell, nodeInfo) {
   // Get the header & column id
-  const headerId = getNodeindex(matrixCell) - 1
-  const columnId = getNodeindex(e.target.closest("tr"))
+  const headerId = getNodeIndex(matrixCell) - 1
+  const columnId = getNodeIndex(matrixCell.closest("tr"))
   const edgeInfoPopup = document.querySelector('.edge-info-popup')
 
   // Fill the popup with data
@@ -35,12 +35,38 @@ function enterCell(e, matrixCell, nodeInfo) {
 
 /**
  * Removes the popup that displays when hovering a cell in the adjacency matrix
- * @param {Event} e The hover event
  */
-function exitCell(e) {
+function exitCell() {
   // Hide the popup
   const edgeInfoPopup = document.querySelector('.edge-info-popup')
   edgeInfoPopup.classList.remove('show')
+}
+
+
+/**
+ * Opens a modal that displays all of the emails in a list
+ * @param {HTMLElement} matrixCell The cell that was hovered
+ * @param {Array.<Object>} nodeInfo An array of node objects
+ */
+function clickCell(matrixCell, nodeInfo) {
+  // Get the header & column id
+  const headerId = getNodeIndex(matrixCell) - 1
+  const columnId = getNodeIndex(matrixCell.closest("tr"))
+
+  openModal(`
+    <h1>Hello world!</h1>
+    <div class="modal-body">
+      <p>This is called a "modal" — A fullscreen popup that darkens the background and is intended to grab the user's full attention.</p>
+      <br />
+      <p>In the case that there are multiple emails between people, it's difficult to display all of them in the hover popup. The concept idea of this modal is to host a table with all of the emails between these 2 people and their corresponding sentiment value.</p>
+      <br />
+      <p>This modal already has access to nodeInfo data! It knows that the cell you pressed corresponds to the sender ${nodeInfo[columnId].id} and the receiver ${nodeInfo[headerId].id}.
+    </div>
+  `)
+
+  // Close the modal if someone clicks outside of it
+  const overlay = document.querySelector('.overlay')
+  overlay.addEventListener('click', () => closeModal(300))
 }
 
 
@@ -67,4 +93,37 @@ function toggleEmails(state, nodeInfo) {
 }
 
 // A function to find a node's index within its parent element
-const getNodeindex = elm => [...elm.parentNode.children].findIndex(c => c == elm)
+const getNodeIndex = el => [...el.parentNode.children].indexOf(el)
+
+
+
+function openModal(html) {
+  // Initialize variables
+  const overlay = document.querySelector('.overlay')
+  const modal = document.querySelector('.modal');
+  const modalContent = modal.querySelector('.modal-content')
+
+  // Render the provided HTML into the modal
+  modalContent.innerHTML = html
+
+  // Display the modal & overlay
+  overlay.style.display = 'block'
+  modal.style.display = 'flex'
+  setTimeout(() => overlay.classList.add('show'), 100)
+  setTimeout(() => modal.classList.add('show'), 100)
+}
+
+function closeModal(animDuration) {
+  // Initialize variables
+  const overlay = document.querySelector('.overlay')
+  const modal = document.querySelector('.modal')
+
+  // Close the modal & overlay
+  overlay.classList.remove('show')
+  modal.classList.remove('show')
+
+  setTimeout(() => {
+    overlay.style.display = 'none'
+    modal.style.display = 'none'
+  }, animDuration)
+}
